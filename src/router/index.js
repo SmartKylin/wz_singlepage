10fbfda3fdf16a9f02acc71c83ead00d4a6c634d
GayeChen <1580850542@qq.com>
2017-09-25 18:49:49 +0800

    跟新readme

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 
-
 export default class RouterMap extends Component {
   render() {
     return (
